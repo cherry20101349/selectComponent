e5ec69a1fe1beb1b9451bf47fd3850c5f2592113
wuyp <wuyp@fsmeeting.com>
2020-09-27 12:13:14 +0800

    点击其他地方隐藏列表

--- select.js
+++ select.js
@@ -17,7 +17,7 @@ Vue.component('el-select', {
                                 placeholder: this.storePlaceholder
                             },
                             domProps: {
-                                placeholder: this.value
+                                value: this.value
                             },
                             on: {
                                 input: function (event) {
@@ -29,9 +29,9 @@ Vue.component('el-select', {
                                     });
                                     self.isShowEmpty = !self.storeList.length;
                                     // 选中li的时候判断是否非选中的li，是的话再$emit('change', value)
-                                    // self.$emit('input', value);
+                                    self.$emit('input', value);
                                 },
-                                focus: function () {
+                                focus: function (event) {
                                     self.storePlaceholder = self.storeSelect || self.placeholder;
                                     self.storeList = self.options;
                                     self.isFocus = true;
@@ -40,46 +40,45 @@ Vue.component('el-select', {
                                         self.$emit('input', '');
                                     }
                                 },
-                                blur: function () {
-                                    //self.isFocus = false;
+                                click: function (event) {
+                                    event.stopPropagation()
                                 }
                             }
                         })
                     ]
                 ),
                 h('div', {
-                    'class': ['el-select-dropdown', {'visible': this.isShowList}]
+                    class: ['el-select-dropdown', {'visible': this.isShowList}]
                 }, [
                     h('ul', {
-                        'class': 'el-select-dropdown__list',
-                        'style': {'display': !self.isShowEmpty ? 'block' : 'none'}
+                        class: 'el-select-dropdown__list',
+                        style: {'display': !self.isShowEmpty ? 'block' : 'none'}
                     }, this.storeList.map(function (item) {
                         return h('li', {
-                            'class': ['el-select-dropdown__item', {'selected': self.storeSelect === item.label}],
-                            'attrs': {
-                                'data-type': item.value
-                            },
+                            class: ['el-select-dropdown__item', {'selected': self.storeSelect === item.label}],
+                            attrs: {'data-type': item.value},
                             on: {
                                 click: function (event) {
+                                    event.stopPropagation();
                                     self.isShowList = false;
                                     self.storeSelect = event.target.innerText;
-                                    self.$emit('change', event.target.innerText, event.target.dataset.type);
+                                    self.$emit('input', event.target.innerText, event.target.dataset.type);
                                 }
                             }
                         }, [item.label])
                     })),
                     h('p', {
-                        'class': 'el-select-dropdown__empty',
-                        'style': {'display': self.isShowEmpty ? 'block' : 'none'}
+                        class: 'el-select-dropdown__empty',
+                        style: {'display': self.isShowEmpty ? 'block' : 'none'}
                     }, ['暂无匹配数据'])
                 ])
             ]
         )
     },
-    model: {
-        prop: 'value',
-        event: 'change'
-    },
+    //model: {
+    //    prop: 'value',
+    //    event: 'change'
+    //},
     props: {
         // input value
         value: {
@@ -104,11 +103,22 @@ Vue.component('el-select', {
             storeSelect: '',// 储存选中的option
             storePlaceholder: '',
             isShowList: false,// 是否显示select列表
-            isShowEmpty: false,// 是否显示无匹配数据
+            isShowEmpty: false// 是否显示无匹配数据
         }
     },
     created: function () {
+        var _that = this;
         this.storeList = this.options;
-        this.storePlaceholder = this.placeholder
+        this.storePlaceholder = this.placeholder;
+        document.body.addEventListener('click', _that.listDisShow);
+    },
+    methods: {
+        listDisShow: function () {
+            this.isShowList = false;
+            this.isShowEmpty = false;
+            if (!this.storeSelect) {
+                this.$emit('input', '');
+            }
+        }
     }
 });
